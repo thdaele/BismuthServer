6133ebd6043fb935bc19d7fea1ab0ec461b286d2
nessie <nessie@hotmail.dk>
2019-09-22 18:11:13 +0200

    Add duration rockets to recipe book.

--- src/main/java/carpet/bismuth/CarpetServer.java
+++ src/main/java/carpet/bismuth/CarpetServer.java
@@ -2,12 +2,41 @@ package carpet.bismuth;
 
 import carpet.bismuth.logging.LoggerRegistry;
 import carpet.bismuth.utils.HUDController;
+import com.google.common.collect.Lists;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.NonNullList;
 
 public class CarpetServer // static for now - easier to handle all around the code, its one anyways
 {
 	public static MinecraftServer minecraft_server;
+
+	private static final IRecipe duration1 = new ShapelessRecipes("rocket", makeFirework(1), NonNullList.from(Ingredient.EMPTY, Ingredient.fromItems(Items.PAPER), Ingredient.fromItems(Items.GUNPOWDER)));
+	private static final IRecipe duration2 = new ShapelessRecipes("rocket", makeFirework(2), NonNullList.from(Ingredient.EMPTY, Ingredient.fromItems(Items.PAPER), Ingredient.fromItems(Items.GUNPOWDER), Ingredient.fromItems(Items.GUNPOWDER)));
+	private static final IRecipe duration3 = new ShapelessRecipes("rocket", makeFirework(3), NonNullList.from(Ingredient.EMPTY, Ingredient.fromItems(Items.PAPER), Ingredient.fromItems(Items.GUNPOWDER), Ingredient.fromItems(Items.GUNPOWDER), Ingredient.fromItems(Items.GUNPOWDER)));
+
+	static {
+		CraftingManager.register("carpet:durationone", duration1);
+		CraftingManager.register("carpet:durationtwo", duration2);
+		CraftingManager.register("carpet:durationthree", duration3);
+	}
+
+	private static ItemStack makeFirework(int duration) {
+		final NBTTagCompound durationTag = new NBTTagCompound();
+		final NBTTagCompound fireworksTag = new NBTTagCompound();
+		durationTag.setByte("Flight", (byte) duration);
+		fireworksTag.setTag("Fireworks", durationTag);
+		final ItemStack firework = new ItemStack(Items.FIREWORKS, 3);
+		firework.setTagCompound(fireworksTag);
+		return firework;
+	}
 
 	public static void init(MinecraftServer server) //aka constructor of this static singleton class
 	{
@@ -24,9 +53,14 @@ public class CarpetServer // static for now - easier to handle all around the co
 
 	public static void playerConnected(EntityPlayerMP player) {
 		LoggerRegistry.playerConnected(player);
+		unlockCustomRecipes(player);
 	}
 
 	public static void playerDisconnected(EntityPlayerMP player) {
 		LoggerRegistry.playerDisconnected(player);
+	}
+
+	private static void unlockCustomRecipes(EntityPlayerMP player) {
+		player.unlockRecipes(Lists.newArrayList(duration1, duration2, duration3));
 	}
 }
