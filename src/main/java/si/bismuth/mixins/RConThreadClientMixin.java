4a767f4e6b3f2659800839b9893ab0e7a116e3c2
nessie <nessie@hotmail.dk>
2019-09-27 05:03:42 +0200

    Fix RCON thread unsafety (probably).

---
+++ src/main/java/si/bismuth/mixins/RConThreadClientMixin.java
@@ -0,0 +1,101 @@
+package si.bismuth.mixins;
+
+import net.minecraft.network.rcon.IServer;
+import net.minecraft.network.rcon.RConThreadBase;
+import net.minecraft.network.rcon.RConThreadClient;
+import net.minecraft.network.rcon.RConUtils;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import si.bismuth.MCServer;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+@Mixin(RConThreadClient.class)
+public abstract class RConThreadClientMixin extends RConThreadBase {
+	// @formatter:off
+	@Shadow private boolean loggedIn;
+	@Shadow private Socket clientSocket;
+	@Shadow @Final private byte[] buffer;
+	@Shadow @Final private String rconPassword;
+	@Shadow @Final private static Logger LOGGER;
+	@Shadow protected abstract void closeSocket();
+	@Shadow protected abstract void sendLoginFailedResponse();
+	@Shadow protected abstract void sendResponse(int id, int type, String message);
+	@Shadow protected abstract void sendMultipacketResponse(int id, String message);
+	// @formatter:on
+
+	protected RConThreadClientMixin(IServer serverIn, String threadName) {
+		super(serverIn, threadName);
+	}
+
+	/**
+	 * @author nessie
+	 * Fixes MC-72390
+	 */
+	@Overwrite
+	public void run() {
+		while (true) {
+			try {
+				if (!this.running) {
+					break;
+				}
+
+				final BufferedInputStream stream = new BufferedInputStream(this.clientSocket.getInputStream());
+				final int i = stream.read(this.buffer, 0, this.buffer.length);
+				if (i >= 10) {
+					final int k = RConUtils.getBytesAsLEInt(this.buffer, 0, i);
+					if (k != i - 4) {
+						break;
+					}
+
+					final int id = RConUtils.getBytesAsLEInt(this.buffer, 4, i);
+					final int type = RConUtils.getRemainingBytesAsLEInt(this.buffer, 8);
+					switch (type) {
+						case 2:
+							if (this.loggedIn) {
+								final String command = RConUtils.getBytesAsString(this.buffer, 12, i);
+
+								MCServer.minecraft_server.addScheduledTask(() -> {
+									try {
+										sendMultipacketResponse(id, server.handleRConCommand(command));
+									} catch (Exception exception) {
+										sendMultipacketResponse(id, "Error executing: " + command + " (" + exception.getMessage() + ")");
+									}
+								});
+
+								continue;
+							}
+
+							this.sendLoginFailedResponse();
+							break;
+						case 3:
+							final String s = RConUtils.getBytesAsString(this.buffer, 12, i);
+							if (!s.isEmpty() && s.equals(this.rconPassword)) {
+								this.loggedIn = true;
+								this.sendResponse(id, 2, "");
+								continue;
+							}
+
+							this.loggedIn = false;
+							this.sendLoginFailedResponse();
+							break;
+						default:
+							this.sendMultipacketResponse(id, String.format("Unknown request %s", Integer.toHexString(type)));
+							break;
+					}
+				}
+			} catch (IOException exception) {
+				break;
+			} catch (Exception exception) {
+				LOGGER.error("Exception whilst parsing RCON input", exception);
+				break;
+			}
+		}
+		this.closeSocket();
+	}
+}
