51680879beff2a3ab0011b7421bc631b69414f7b
nessie <nessie@hotmail.dk>
2020-03-03 20:07:09 +0100

    Properly disable the server GUI…

--- src/main/java/si/bismuth/mixins/MixinDedicatedServer.java
+++ src/main/java/si/bismuth/mixins/MixinDedicatedServer.java
@@ -1,7 +1,6 @@
 package si.bismuth.mixins;
 
 import net.minecraft.server.dedicated.DedicatedServer;
-import net.minecraft.server.gui.MinecraftServerGui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,7 +16,6 @@ public abstract class MixinDedicatedServer {
 	 */
 	@Overwrite
 	public void setGuiEnabled() {
-		MinecraftServerGui.createServerGui((DedicatedServer) (Object) this);
 		this.guiIsEnabled = false;
 	}
 }
