0ee484eff6dc36fb5483bee6a0b01562ee4cae67
nessie <nessie@hotmail.dk>
2020-04-17 18:08:48 +0200

    let players use creative mode on debug servers.

--- src/main/java/si/bismuth/mixins/MixinCommandGameMode.java
+++ src/main/java/si/bismuth/mixins/MixinCommandGameMode.java
@@ -7,11 +7,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import si.bismuth.MCServer;
 
 @Mixin(CommandGameMode.class)
 public abstract class MixinCommandGameMode {
 	@Inject(method = "getGameModeFromCommand", at = @At("RETURN"), cancellable = true)
 	private void onGetGameModeFromCommand(ICommandSender sender, String mode, CallbackInfoReturnable<GameType> cir) {
+		if (!MCServer.server.isServerInOnlineMode()) {
+			return;
+		}
+
 		if (cir.getReturnValue() == GameType.CREATIVE) {
 			cir.setReturnValue(GameType.SPECTATOR);
 		} else if (cir.getReturnValue() == GameType.ADVENTURE) {
