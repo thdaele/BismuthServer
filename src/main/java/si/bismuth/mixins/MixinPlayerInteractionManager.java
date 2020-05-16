thdaele/BismuthServer

commit af045f6c1f8588d68a70b4e54a47d0e570525b46

Show player advancements on Discord, disable login timeout, and some minor bikeshedding and refactoring.

src/main/java/si/bismuth/mixins/MixinPlayerInteractionManager.java
@@ -22,21 +22,17 @@ import si.bismuth.utils.BlockRotator;
 public abstract class MixinPlayerInteractionManager {
 	@Shadow
 	public World world;
-
 	@Shadow
 	public EntityPlayerMP player;
 
-	@Shadow
-	private BlockPos destroyPos = BlockPos.ORIGIN;
-
 	@Inject(method = "onBlockClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;sendBlockBreakProgress(ILnet/minecraft/util/math/BlockPos;I)V", shift = At.Shift.BEFORE))
 	private void notifyUpdate(BlockPos pos, EnumFacing face, CallbackInfo ci) {
-		this.player.connection.sendPacket(new SPacketBlockChange(world, destroyPos));
+		this.player.connection.sendPacket(new SPacketBlockChange(world, BlockPos.ORIGIN));
 	}
 
 	@Redirect(method = "processRightClickBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;onBlockActivated(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/util/EnumHand;Lnet/minecraft/util/EnumFacing;FFF)Z"))
 	private boolean onProcessRightClickBlock(Block block, World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing face, float hitX, float hitY, float hitZ) {
-		boolean flipped = BlockRotator.flipBlockWithCactus(world, pos, state, player, face, hitX, hitY, hitZ);
+		final boolean flipped = BlockRotator.flipBlockWithCactus(world, pos, state, player, face, hitX, hitY, hitZ);
 		if (flipped) {
 			return true;
 		}

src/main/java/si/bismuth/discord/DCBot.java
@@ -56,7 +56,11 @@ public class DCBot extends ListenerAdapter {
 		}
 	}
 
-	public void sendDeathmessage(ITextComponent component) {
+	public void sendAdvancementMessage(ITextComponent component) {
+		this.sendToDiscord("**\uD83C\uDF8A " + component.getUnformattedText() + "**");
+	}
+
+	public void sendDeathMessage(ITextComponent component) {
 		this.sendToDiscord("**\uD83D\uDD71 " + component.getUnformattedText() + "**");
 	}
 

src/main/java/si/bismuth/mixins/MixinEntityPlayerMP.java
@@ -86,7 +86,7 @@ public abstract class MixinEntityPlayerMP extends EntityPlayer implements IRecip
 	@Redirect(method = "onDeath", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/management/PlayerList;sendMessage(Lnet/minecraft/util/text/ITextComponent;)V"))
 	private void sendMessage(PlayerList list, ITextComponent component) {
 		list.sendMessage(component);
-		MCServer.bot.sendDeathmessage(component);
+		MCServer.bot.sendDeathMessage(component);
 		MCServer.log.info("Player {} died at {} {} {} in {}", this.getName(), this.posX, this.posY, this.posZ, this.world.provider.getDimensionType().getName());
 	}
 }

src/main/java/si/bismuth/mixins/MixinEntityTameable.java
@@ -13,6 +13,6 @@ public abstract class MixinEntityTameable {
 	@Redirect(method = "onDeath", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;sendMessage(Lnet/minecraft/util/text/ITextComponent;)V"))
 	private void sendMessage(EntityLivingBase entity, ITextComponent component) {
 		MCServer.server.getPlayerList().sendMessage(component);
-		MCServer.bot.sendDeathmessage(component);
+		MCServer.bot.sendDeathMessage(component);
 	}
 }

src/main/java/si/bismuth/mixins/MixinNetHandlerLoginServer.java
@@ -1,0 +1,14 @@
+package si.bismuth.mixins;
+
+import net.minecraft.server.network.NetHandlerLoginServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(NetHandlerLoginServer.class)
+public abstract class MixinNetHandlerLoginServer {
+	@ModifyConstant(method = "update", constant = @Constant(intValue = 600))
+	private int disableLoginTimeout(int value) {
+		return -1;
+	}
+}

src/main/java/si/bismuth/mixins/MixinPlayerAdvancements.java
@@ -1,0 +1,18 @@
+package si.bismuth.mixins;
+
+import net.minecraft.advancements.PlayerAdvancements;
+import net.minecraft.server.management.PlayerList;
+import net.minecraft.util.text.ITextComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import si.bismuth.MCServer;
+
+@Mixin(PlayerAdvancements.class)
+public abstract class MixinPlayerAdvancements {
+	@Redirect(method = "grantCriterion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/management/PlayerList;sendMessage(Lnet/minecraft/util/text/ITextComponent;)V"))
+	private void onSendAdvancement(PlayerList list, ITextComponent component) {
+		list.sendMessage(component);
+		MCServer.bot.sendAdvancementMessage(component);
+	}
+}

src/main/java/si/bismuth/utils/BlockRotator.java
@@ -25,11 +25,18 @@ import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BlockRotator {
+	private static final List<String> UUIDS = Arrays.asList("c36e8cbd-b090-47b7-8166-bab6985e4382", "73a0e9c7-d30f-43dd-b820-55c72c62a6f7", "78bbb591-b677-41cc-9d60-194fcb2e422c", "8a45fff7-0335-4928-a98c-dcc8b1ad5193");
+
 	public static boolean flipBlockWithCactus(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing facing, float hitX, float hitY, float hitZ) {
 		if (!playerIn.capabilities.allowEdit || !player_holds_cactus_mainhand(playerIn)) {
 			return false;
 		}
+
+		if (UUIDS.contains(String.valueOf(EntityPlayer.getUUID(playerIn.getGameProfile())))) return false;
 		return flip_block(worldIn, pos, state, facing, hitX, hitY, hitZ);
 	}
 
@@ -96,6 +103,7 @@ public class BlockRotator {
 	public static boolean flippinEligibility(Entity entity) {
 		if (entity instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer) entity;
+			if (UUIDS.contains(String.valueOf(EntityPlayer.getUUID(player.getGameProfile())))) return false;
 			return (!player.getHeldItemOffhand().isEmpty() && player.getHeldItemOffhand().getItem() instanceof ItemBlock && ((ItemBlock) (player.getHeldItemOffhand().getItem())).getBlock() == Blocks.CACTUS);
 		}
 		return false;

commit d0a4abd13e9df8f2f33672ebf2cc309213b0433a

Fix ghost block issue caused by https://github.com/Nessiesson/BismuthServer/commit/af045f6c1f8588d68a70b4e54a47d0e570525b46.

src/main/java/si/bismuth/mixins/MixinPlayerInteractionManager.java
@@ -25,9 +25,9 @@ public abstract class MixinPlayerInteractionManager {
 	@Shadow
 	public EntityPlayerMP player;
 
-	@Inject(method = "onBlockClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;sendBlockBreakProgress(ILnet/minecraft/util/math/BlockPos;I)V", shift = At.Shift.BEFORE))
+	@Inject(method = "onBlockClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;sendBlockBreakProgress(ILnet/minecraft/util/math/BlockPos;I)V"))
 	private void notifyUpdate(BlockPos pos, EnumFacing face, CallbackInfo ci) {
-		this.player.connection.sendPacket(new SPacketBlockChange(world, BlockPos.ORIGIN));
+		this.player.connection.sendPacket(new SPacketBlockChange(this.world, pos));
 	}
 
 	@Redirect(method = "processRightClickBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;onBlockActivated(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/util/EnumHand;Lnet/minecraft/util/EnumFacing;FFF)Z"))