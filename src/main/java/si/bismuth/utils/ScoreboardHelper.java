bdc491ca7a7a69d7ad5ba6b42071676849d1da77
thdaele <thomas.vandaele2000@gmail.com>
2022-02-04 20:12:57 +0100

    use more meaningful variablename

--- src/main/java/si/bismuth/utils/ScoreboardHelper.java
+++ src/main/java/si/bismuth/utils/ScoreboardHelper.java
@@ -13,9 +13,9 @@ import java.util.stream.Collectors;
 public class ScoreboardHelper {
 	private static final ServerScoreboard board = (ServerScoreboard) MCServer.server.getWorld(0).getScoreboard();
 
-	public static void setScoreboard(List<String> args, int index) {
+	public static void setScoreboard(List<String> args, int displaySlot) {
 		final ScoreObjective objective = ScoreboardHelper.getObjective(args, board);
-		board.setObjectiveInDisplaySlot(index, objective);
+		board.setObjectiveInDisplaySlot(displaySlot, objective);
 	}
 
 	public static ScoreObjective getObjective(List<String> args, Scoreboard board) {
