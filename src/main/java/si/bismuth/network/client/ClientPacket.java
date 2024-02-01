450a463e02196444707c4793a401744e0d3f0f3c
thdaele <thomas.vandaele2000@gmail.com>
2024-02-01 12:22:45 +0100

    remove unused code

--- src/main/java/si/bismuth/network/client/ClientPacket.java
+++
@@ -1,7 +0,0 @@
-package si.bismuth.network.client;
-
-import si.bismuth.network.BisPacket;
-
-public interface ClientPacket extends BisPacket {
-	void handle();
-}
--- src/main/java/si/bismuth/network/server/ServerPacket.java
+++
@@ -1,8 +0,0 @@
-package si.bismuth.network.server;
-
-import net.minecraft.server.entity.living.player.ServerPlayerEntity;
-import si.bismuth.network.BisPacket;
-
-public interface ServerPacket extends BisPacket {
-	void handle(ServerPlayerEntity player);
-}
