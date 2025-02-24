4cccba355b9b43dc6f3df0adf277ac41f43d87a8
Ashley <backslashashley@outlook.com>
2025-02-25 05:17:39 +1100

    fix shit

--- src/main/java/si/bismuth/mixins/HopperBlockMixin.java
+++ src/main/java/si/bismuth/mixins/HopperBlockMixin.java
@@ -3,15 +3,12 @@ package si.bismuth.mixins;
 import net.minecraft.block.Block;
 import net.minecraft.block.HopperBlock;
 import net.minecraft.block.state.BlockState;
-import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
-import net.minecraft.server.entity.living.player.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import java.util.List;
 
 @Mixin(HopperBlock.class)
 public class HopperBlockMixin {
